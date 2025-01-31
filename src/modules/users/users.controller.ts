f78c323ec91a22e89802deee4ea9d45e5ba8be2b
serhii.ryabko <serhii.ryabko@gmail.com>
2025-01-31 15:43:04 +0200

    updates user columns

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, ParseIntPipe, Patch, Post, UseGuards } from "@nestjs/common";
 import { User } from "@prisma/client";
+import { JwtAccessGuard } from "src/modules/auth/guards";
+import { CurrentUser } from "src/utils";
 import { UsersService } from "./users.service";
-import { CreateUserDto } from "./dto";
+import { CreateUserDto, UpdateUserDto } from "./dto";
 
+@UseGuards(JwtAccessGuard)
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -10,5 +13,18 @@ export class UsersController {
   @Post()
   createOne(@Body() dto: CreateUserDto): Promise<User> {
     return this.usersService.createOne(dto);
+  }
+
+  @Patch()
+  updateOne(
+    @CurrentUser("id", ParseIntPipe) id: number,
+    @Body() dto: UpdateUserDto
+  ): Promise<User> {
+    return this.usersService.updateOne(id, dto);
+  }
+
+  @Get()
+  getOne(@CurrentUser("id", ParseIntPipe) id: number): Promise<User> {
+    return this.usersService.getOne({ id });
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,13 @@
-import { BadRequestException, ConflictException, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
+import { hash } from "argon2";
+import { User } from "@prisma/client";
 import { PrismaService } from "prisma/prisma.service";
-import { CreateUserDto, GetUserDto } from "./dto";
+import { CreateUserDto, GetUserDto, UpdateUserDto } from "./dto";
 
 @Injectable()
 export class UsersService {
@@ -19,5 +26,27 @@ export class UsersService {
     if (!id && !email) throw new BadRequestException();
 
     return this.prismaService.user.findFirst({ where: { id, email } });
+  }
+
+  async updateOne(id: number, dto: UpdateUserDto) {
+    const user = await this.getOneOrThrow(id);
+    const { email, password } = dto;
+
+    if (email) {
+      const userByEmail = await this.getOne({ email });
+      if (userByEmail) throw new ConflictException("User with this email is already existing");
+    }
+
+    let hashedPassword = user.hashedPassword;
+    if (password) hashedPassword = await hash(password);
+
+    return this.prismaService.user.update({ where: { id }, data: { ...dto, hashedPassword } });
+  }
+
+  private async getOneOrThrow(id: number): Promise<User> {
+    const user = await this.getOne({ id });
+    if (!user) throw new NotFoundException("could not find any user");
+
+    return user;
   }
 }
